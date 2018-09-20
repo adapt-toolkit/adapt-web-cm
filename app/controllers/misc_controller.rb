7d5c4679b9f4d9deee73674cce19b79f56a65608
Victor Clime <viclime@gmail.com>
2018-09-20 20:23:22 +0300

    jwt validation

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,9 +1,19 @@
 class MiscController < ApplicationController
-  def token
-    
-  end
   def maintenance
-    `service nginx stop`
-    head :ok
+    token = params.permit(:token).try(:[], :token)
+    decoded_token = JWT.decode token, Rails.application.secrets.hmac_secret, true, { algorithm: 'HS512' }
+    if decoded_token.first.try(:[], "data") == "authorized"
+      `service nginx stop`
+      `service mn_mode start`
+      head :ok
+    else
+      head :unknown_error
+    end
+  rescue JWT::VerificationError
+    head :forbidden
+  rescue JWT::ExpiredSignature
+    head :forbidden
+  rescue
+    head :internal_server_error
   end
 end
