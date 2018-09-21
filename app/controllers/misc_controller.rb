7c1839a03e0732f2d13e18cd9459c8b8c467ffef
Victor Clime <viclime@gmail.com>
2018-09-21 09:52:56 +0300

    csrf and devise auth skip for maintenance switcher

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,8 +1,10 @@
 class MiscController < ApplicationController
+  skip_before_action :authenticate_user!, :verify_authenticity_token, only: :maintenance
+
   def maintenance
-    token = params.permit(:token).try(:[], :token)
+    token = params.try(:[], :token)
     decoded_token = JWT.decode token, Rails.application.secrets.hmac_secret, true, { algorithm: 'HS512' }
-    if decoded_token.first.try(:[], "data") == "authorized"
+    if decoded_token.first.try(:[], "authorized") == "true"
       `service nginx stop`
       `service mn_mode start`
       head :ok
